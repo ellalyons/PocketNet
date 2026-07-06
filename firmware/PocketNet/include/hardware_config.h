76f5c3420de3b1844c6b21ceb23fbb9f5420cd48
ellalyons <raw.wood4715@fastmail.com>
2026-07-06 21:35:46 +1000

    Add RGB LED hardware bring-up test

---
+++ firmware/PocketNet/include/hardware_config.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// RGB LED
+#define PIN_RGB_R 21
+#define PIN_RGB_G 22
+#define PIN_RGB_B 23
+
+// TFT
+#define PIN_TFT_BL 4
+#define PIN_TFT_DC 5
+#define PIN_TFT_RST 18
+#define PIN_TFT_SCK 16
+#define PIN_TFT_MOSI 17
+
+// Buzzer
+#define PIN_BUZZER 2
+
+// Buttons
+#define PIN_LEFT 15
+#define PIN_RIGHT 14
+#define PIN_SELECT 27
+#define PIN_ALT 39
+
+// I2C
+#define PIN_SDA 26
+#define PIN_SCL 25
--- firmware/PocketNet/src/main.cpp
+++ firmware/PocketNet/src/main.cpp
@@ -1,15 +1,35 @@
 #include <Arduino.h>
+#include "hardware_config.h"
 
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
-
-  Serial.println();
-  Serial.println("PocketNet booting...");
-  Serial.println("ESP32 is alive.");
+void setRGB(bool r, bool g, bool b)
+{
+    digitalWrite(PIN_RGB_R, r);
+    digitalWrite(PIN_RGB_G, g);
+    digitalWrite(PIN_RGB_B, b);
 }
 
-void loop() {
-  Serial.println("PocketNet heartbeat");
-  delay(1000);
+void setup()
+{
+    Serial.begin(115200);
+
+    pinMode(PIN_RGB_R, OUTPUT);
+    pinMode(PIN_RGB_G, OUTPUT);
+    pinMode(PIN_RGB_B, OUTPUT);
+
+    Serial.println("PocketNet Boot");
+}
+
+void loop()
+{
+    Serial.println("Red");
+    setRGB(HIGH, LOW, LOW);
+    delay(1000);
+
+    Serial.println("Green");
+    setRGB(LOW, HIGH, LOW);
+    delay(1000);
+
+    Serial.println("Blue");
+    setRGB(LOW, LOW, HIGH);
+    delay(1000);
 }
