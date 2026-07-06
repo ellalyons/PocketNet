c3e271af9818e5f773a199ebdaa4b2fba72e1fdc
ellalyons <raw.wood4715@fastmail.com>
2026-07-06 21:46:29 +1000

    Add hardware bring-up tests for RGB LED and buzzer

--- firmware/PocketNet/src/main.cpp
+++ firmware/PocketNet/src/main.cpp
@@ -8,28 +8,55 @@ void setRGB(bool r, bool g, bool b)
     digitalWrite(PIN_RGB_B, b);
 }
 
+void beep(int durationMs)
+{
+    digitalWrite(PIN_BUZZER, HIGH);
+    delay(durationMs);
+    digitalWrite(PIN_BUZZER, LOW);
+}
+
 void setup()
 {
     Serial.begin(115200);
+    delay(1000);
 
+    Serial.println();
+    Serial.println("=================================");
+    Serial.println(" PocketNet Hardware Bring-up");
+    Serial.println("=================================");
+
+    // RGB LED
     pinMode(PIN_RGB_R, OUTPUT);
     pinMode(PIN_RGB_G, OUTPUT);
     pinMode(PIN_RGB_B, OUTPUT);
 
-    Serial.println("PocketNet Boot");
+    // Buzzer
+    pinMode(PIN_BUZZER, OUTPUT);
+
+    // Turn everything off
+    setRGB(LOW, LOW, LOW);
+    digitalWrite(PIN_BUZZER, LOW);
 }
 
 void loop()
 {
-    Serial.println("Red");
+    Serial.println("Testing RED");
     setRGB(HIGH, LOW, LOW);
     delay(1000);
 
-    Serial.println("Green");
+    Serial.println("Testing GREEN");
     setRGB(LOW, HIGH, LOW);
     delay(1000);
 
-    Serial.println("Blue");
+    Serial.println("Testing BLUE");
     setRGB(LOW, LOW, HIGH);
     delay(1000);
+
+    Serial.println("Testing OFF");
+    setRGB(LOW, LOW, LOW);
+    delay(500);
+
+    Serial.println("Testing BUZZER");
+    beep(100);
+    delay(900);
 }
