76a72d21bae32398c26ba333290e4fe38a37f15d
ellalyons <raw.wood4715@fastmail.com>
2026-07-07 21:48:57 +1000

    Add TFT display bring-up test

--- firmware/PocketNet/src/main.cpp
+++ firmware/PocketNet/src/main.cpp
@@ -1,81 +1,91 @@
 #include <Arduino.h>
+#include <LovyanGFX.hpp>
 #include "hardware_config.h"
 
-void setRGB(bool r, bool g, bool b)
+class LGFX : public lgfx::LGFX_Device
 {
-    digitalWrite(PIN_RGB_R, r);
-    digitalWrite(PIN_RGB_G, g);
-    digitalWrite(PIN_RGB_B, b);
-}
+  lgfx::Panel_ST7735S _panel;
+  lgfx::Bus_SPI _bus;
 
-void beep(int durationMs)
-{
-    digitalWrite(PIN_BUZZER, HIGH);
-    delay(durationMs);
-    digitalWrite(PIN_BUZZER, LOW);
-}
+public:
+  LGFX()
+  {
+    {
+      auto cfg = _bus.config();
+      cfg.spi_host = VSPI_HOST;
+      cfg.spi_mode = 0;
+      cfg.freq_write = 40000000;
+      cfg.freq_read = 40000000;
+      cfg.spi_3wire = false;
+      cfg.use_lock = true;
+      cfg.dma_channel = 1;
+      cfg.pin_sclk = PIN_TFT_SCK;
+      cfg.pin_mosi = PIN_TFT_MOSI;
+      cfg.pin_miso = -1;
+      cfg.pin_dc = PIN_TFT_DC;
+      _bus.config(cfg);
+      _panel.setBus(&_bus);
+    }
+
+    {
+      auto cfg = _panel.config();
+      cfg.pin_cs = -1;
+      cfg.pin_rst = PIN_TFT_RST;
+      cfg.pin_busy = -1;
+
+      cfg.memory_width = 132;
+      cfg.memory_height = 132;
+      cfg.panel_width = 128;
+      cfg.panel_height = 128;
+      cfg.offset_x = 2;
+      cfg.offset_y = 1;
+      cfg.offset_rotation = 2;
+
+      cfg.readable = false;
+      cfg.invert = false;
+      cfg.rgb_order = false;
+      cfg.dlen_16bit = false;
+      cfg.bus_shared = false;
+
+      _panel.config(cfg);
+    }
+
+    setPanel(&_panel);
+  }
+};
+
+LGFX display;
 
 void setup()
 {
-    Serial.begin(115200);
-    delay(1000);
+  Serial.begin(115200);
+  delay(1000);
 
-    Serial.println();
-    Serial.println("=================================");
-    Serial.println(" PocketNet Input Bring-up");
-    Serial.println("=================================");
+  Serial.println("PocketNet Display Bring-up");
 
-    pinMode(PIN_RGB_R, OUTPUT);
-    pinMode(PIN_RGB_G, OUTPUT);
-    pinMode(PIN_RGB_B, OUTPUT);
-    pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(PIN_TFT_BL, OUTPUT);
+  digitalWrite(PIN_TFT_BL, LOW);
 
-    pinMode(PIN_LEFT, INPUT);
-    pinMode(PIN_RIGHT, INPUT);
-    pinMode(PIN_SELECT, INPUT);
-    pinMode(PIN_ALT, INPUT);
+  display.init();
+  display.setRotation(0);
+  display.fillScreen(TFT_BLACK);
 
-    setRGB(LOW, LOW, LOW);
-    digitalWrite(PIN_BUZZER, LOW);
+  display.setTextColor(TFT_GREEN, TFT_BLACK);
+  display.setTextSize(2);
+  display.setCursor(10, 20);
+  display.println("PocketNet");
 
-    Serial.println("Press buttons / move joystick...");
+  display.setTextColor(TFT_WHITE, TFT_BLACK);
+  display.setTextSize(1);
+  display.setCursor(10, 55);
+  display.println("Display OK");
+
+  display.setCursor(10, 75);
+  display.println("RGB + Buzzer OK");
+
+  Serial.println("Display test complete");
 }
 
 void loop()
 {
-    bool leftPressed   = digitalRead(PIN_LEFT);
-    bool rightPressed  = digitalRead(PIN_RIGHT);
-    bool selectPressed = digitalRead(PIN_SELECT);
-    bool altPressed    = digitalRead(PIN_ALT);
-
-    if (leftPressed) {
-        Serial.println("LEFT pressed");
-        setRGB(HIGH, LOW, LOW);
-        beep(50);
-        delay(250);
-    }
-
-    if (rightPressed) {
-        Serial.println("RIGHT pressed");
-        setRGB(LOW, HIGH, LOW);
-        beep(50);
-        delay(250);
-    }
-
-    if (selectPressed) {
-        Serial.println("SELECT pressed");
-        setRGB(LOW, LOW, HIGH);
-        beep(50);
-        delay(250);
-    }
-
-    if (altPressed) {
-        Serial.println("BUTTON pressed");
-        setRGB(HIGH, HIGH, HIGH);
-        beep(50);
-        delay(250);
-    }
-
-    setRGB(LOW, LOW, LOW);
-    delay(20);
 }
